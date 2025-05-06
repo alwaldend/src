339c26084e9a57a529427060c2dbe53e157fc036
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-05-06 12:46:36 +0300

    Combine scripts into an archive, use heredoc instead of string manipulation

--- shell/scripts/build-install-script.sh
+++ shell/scripts/build-install-script.sh
@@ -2,23 +2,28 @@
 
 set -eux
 
-result="#!/usr/bin/env sh\n"
-result+="set -eux\n"
-output=${1}
-shift
-for file in "${@}"; do
-    base64=$(base64 --wrap 0 "${file}") 
-    name=$(basename "${file}")
-    name="${name%.sh}"
-    if [ "${name}" = "bashrc"; then
+archive="${1}"
+output=${2}
+
+cat - >"${output}" <<EOF
+#!/usr/bin/env sh
+set -eux
+temp=\$(mktemp -d)
+archive="\${temp}/archive.tar"
+trap 'rm -r "\${temp}"' EXIT
+
+echo '$(base64 --wrap 0 "${1}")' | base64 --decode >"\${archive}"
+tar -xf "\${archive}" -C "\${temp}"
+for script in "\${temp}"/*.sh; do
+    name=\$(basename "\${script}")
+    name="\${name%.sh}"
+    if [ "\${name}" = "bashrc" ]; then
         target="\${HOME}/.bashrc"
     else
-        target="\${HOME}/.local/bin/${name}"
+        target="\${HOME}/.local/bin/\${name}"
     fi
-    result+="\n# ${name}\n"
-    result+="mkdir -p \"\$(dirname \"${target}\")\"\n"
-    result+="base64='${base64}'\n"
-    result+="base64 --decode <<<\"${base64}\" >\"${target}\"\n"
-    result+="chmod u+x \"${target}\""
+    mkdir -p "\$(dirname "\${target}")"
+    mv "\${script}" "\${target}"
+    chmod u+x "\${target}"
 done
-echo -e "${result}" >"${output}"
+EOF
