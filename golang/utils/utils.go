d4b39727e2399a7a6ca8ea00a16f80301cfcf612
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-05-15 19:59:51 +0300

    Enable `--worker_sandboxing`

--- golang/utils/utils.go
+++ golang/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"io"
 )
 
+// convert io.Reader to []byte
 func StreamToByte(stream io.Reader) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(stream)
@@ -14,6 +15,7 @@ func StreamToByte(stream io.Reader) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// convert io.Reader to string
 func StreamToString(stream io.Reader) (string, error) {
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(stream)
