1c8085a0ba41551516600813ebb2ceac53700df4
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-05-31 15:19:51 +0300

    Add getopt to bazel-tool-runner, add copts, add brace insertion

--- c/bazel-tool-runner/main.c
+++ c/bazel-tool-runner/main.c
@@ -1,7 +1,33 @@
+#include <ctype.h>
 #include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-    printf("running tool: %s\n", argv[0]);
+struct Config {
+    char* output_file;
+};
+
+int main(int argc, char** argv) {
+    int option_character;
+    while ((option_character = getopt(argc, argv, "o:")) != -1) {
+        switch (option_character) {
+            case 'o':
+                break;
+            case '?':
+                if (optopt == 'c') {
+                    fprintf(stderr, "Option -%c requires an argument.\n",
+                            optopt);
+                } else if (isprint(optopt)) {
+                    fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option character `\\x%x'.\n",
+                            optopt);
+                }
+                return 1;
+            default:
+                abort();
+        }
+    }
     return 0;
 }
