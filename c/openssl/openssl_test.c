64228eaca33f8bd87822f009680318407b3c35b9
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-06-12 19:52:48 +0300

    Add c/utils/base64

--- c/openssl/openssl_test.c
+++ c/openssl/openssl_test.c
@@ -1,70 +1,53 @@
 #include <openssl/evp.h>
-#include <openssl/sha.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int digest_message(const unsigned char *message, size_t message_len,
-                   unsigned char **digest, unsigned int *digest_len) {
+int main(int argc, char *argv[]) {
     EVP_MD_CTX *mdctx;
-    const EVP_MD *evp_md = EVP_sha256();
-    int err = 1;
-
-    mdctx = EVP_MD_CTX_new();
-    if (mdctx == NULL) {
-        err = 0;
-    }
-    if (err != 1) {
-        err = EVP_DigestInit_ex(mdctx, evp_md, NULL);
-    }
-    if (err != 1) {
-        err = EVP_DigestUpdate(mdctx, message, message_len);
-    }
-    if (err != 1) {
-        *digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(evp_md));
-        if (digest == NULL) {
-            err = 0;
-        }
-    }
-    if (err != 1) {
-        err = EVP_DigestFinal_ex(mdctx, *digest, digest_len);
-    }
-
-    EVP_MD_CTX_free(mdctx);
-    if (err == 1) {
-        return 0;
-    }
-    return 1 + err;
-}
-
-int main(int argc, char **argv) {
+    const EVP_MD *md;
+    char message[] = "Hello World\n";
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len, i;
     if (argc != 1) {
-        char output[BUFSIZ];
-        int i;
-        for (i = 0; i < argc; i++) {
-            strcat(output, "\n");
-            strcat(output, argv[i]);
+        fprintf(stderr, "did not expect args: ");
+        for (i = 0; i < (unsigned int)argc; i++) {
+            fprintf(stderr, "%s", argv[i]);
         }
-        fprintf(stderr, "there should be no arguments, got %d: %s\n", argc,
-                output);
-        printf("there should be no arguments, got %d: %s\n", argc, output);
+        fprintf(stderr, "\n");
         return 1;
     }
 
-    unsigned char *message_hash_calc[SHA256_DIGEST_LENGTH];
-    unsigned int *message_hash_length = NULL;
-    static const unsigned char message[12] = "hello world";
-    static const unsigned char message_hash[100] =
-        "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9";
-    int result;
-    result = digest_message(message, sizeof(message), message_hash_calc,
-                            message_hash_length);
-    if (result != 0) {
-        fprintf(stderr,
-                "message hash '%s' is not equal to calculated hash '%s': "
-                "return code '%d'\n",
-                message_hash, message_hash_calc[0], result);
+    OpenSSL_add_all_digests();
+
+    md = EVP_get_digestbyname("sha256");
+
+    if (!md) {
+        fprintf(stderr, "Unknown message digest\n");
         return 1;
     }
+
+    mdctx = EVP_MD_CTX_create();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestUpdate(mdctx, message, strlen(message));
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+    EVP_MD_CTX_destroy(mdctx);
+
+    fprintf(stderr, "Digest is:\n");
+    for (i = 0; i < md_len; i++) {
+        fprintf(stderr, "%02x", md_value[i]);
+    }
+    fprintf(stderr, "\n");
+
+    BIO *bio, *b64;
+
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_new_fp(stderr, BIO_NOCLOSE);
+    BIO_push(b64, bio);
+    BIO_write(b64, md_value, md_len);
+    BIO_flush(b64);
+
+    BIO_free_all(b64);
+
+    EVP_cleanup();
     return 0;
 }
