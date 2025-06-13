73a3115cdaca3230db8430216ccd70317a477615
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-06-13 18:03:03 +0300

    Rewrite c/sri to accept files instead of strings

--- c/sri/main.c
+++ c/sri/main.c
@@ -1,17 +1,51 @@
+#include <ctype.h>
 #include <err.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "c/sri/errors.h"
 #include "c/sri/sri.h"
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        fprintf(stderr, "invalid argument count\n");
+int main(int argc, char **argv) {
+    char *file_path = NULL;
+    char *digest_type = NULL;
+    char flag;
+    FILE *file;
+
+    while ((flag = getopt(argc, argv, "d:f:")) != -1) {
+        switch (flag) {
+            case 'd':
+                digest_type = optarg;
+                break;
+            case 'f':
+                file_path = optarg;
+                break;
+            case '?':
+                if (isprint(optopt)) {
+                    fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option character '\\x%x'.\n",
+                            optopt);
+                }
+                return 1;
+            default:
+                abort();
+        }
+    }
+    file = fopen(file_path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "could not open file %s: %s", file_path,
+                strerror(errno));
         return 1;
     }
-    if (!sri_write(argv[1], stdout, argv[2])) {
+    if (!sri_write(digest_type, file, stdout)) {
+        fclose(file);
         errors_print(stderr);
         return 1;
     }
+    fclose(file);
     return 0;
 }
--- c/sri/sri.h
+++ c/sri/sri.h
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool sri_calculate(char *digest_type, char *in, unsigned int in_length,
-                   unsigned char *out, unsigned int *out_length);
+bool sri_calculate(char *digest_type, FILE *in, unsigned char *out,
+                   unsigned int *out_length);
 
-bool sri_write(char *digest_type, FILE *stream, char *in);
+bool sri_write(char *digest_type, FILE *in, FILE *out);
