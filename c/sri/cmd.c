4fd95ea4c133520be295429c4ced070678520d02
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-07-25 16:37:59 +0300

    Fix segfaults in c/sri, add more test cases

--- c/sri/cmd.c
+++ c/sri/cmd.c
@@ -16,14 +16,20 @@ static struct argp_option options[] = {
      .key = 'f',
      .arg = "Path",
      .doc = "Path to the file to parse"},
+    {.name = "output",
+     .key = 'o',
+     .arg = "Path",
+     .doc = "Output file path (default: stdout)"},
+    {0},
 };
 
 struct arguments {
     char *digest;
     char *file;
+    char *output_file;
 };
 
-static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+static error_t argp_parser(int key, char *arg, struct argp_state *state) {
     struct arguments *arguments = state->input;
     switch (key) {
         case 'd':
@@ -32,25 +38,31 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
         case 'f':
             arguments->file = arg;
             break;
+        case 'o':
+            arguments->output_file = arg;
+            break;
+        case ARGP_KEY_ARG:
+            if (state->arg_num != 0) {
+                fprintf(stderr, "arguments are not supported\n");
+                return 1;
+            }
         case ARGP_KEY_END:
             if (arguments->digest == NULL) {
                 fprintf(stderr, "missing digest\n");
-                return ARGP_ERR_UNKNOWN;
+                return 1;
             }
             if (arguments->file == NULL) {
                 fprintf(stderr, "missing file\n");
-                return ARGP_ERR_UNKNOWN;
+                return 1;
             }
             break;
-        default:
-            return ARGP_ERR_UNKNOWN;
     }
     return 0;
 }
 
 static struct argp argp = {
     .options = options,
-    .parser = parse_opt,
+    .parser = argp_parser,
     .doc =
         "Generate sri of a file \n\
 \n\
@@ -60,23 +72,40 @@ Example:\n\
 };
 
 int cmd_run(int argc, char **argv) {
-    FILE *file;
-    struct arguments arguments = {.digest = NULL, .file = NULL};
-    if (!argp_parse(&argp, argc, argv, 0, 0, &arguments)) {
+    FILE *input_file, *output_file;
+    struct arguments arguments = {
+        .digest = NULL,
+        .file = NULL,
+        .output_file = NULL,
+    };
+    if (0 != argp_parse(&argp, argc, argv, 0, 0, &arguments)) {
         fprintf(stderr, "could not parse arguments\n");
         return 1;
     };
-    file = fopen(arguments.file, "r");
-    if (file == NULL) {
-        fprintf(stderr, "could not open file %s: %s", arguments.file,
+    input_file = fopen(arguments.file, "r");
+    if (input_file == NULL) {
+        fprintf(stderr, "could not open input file %s: %s\n", arguments.file,
                 strerror(errno));
         return 1;
     }
-    if (!sri_write(arguments.digest, file, stdout)) {
-        fclose(file);
+    if (arguments.output_file == NULL) {
+        output_file = stdout;
+    } else {
+        output_file = fopen(arguments.output_file, "w");
+        if (output_file == NULL) {
+            fprintf(stderr, "could not open output file %s: %s\n",
+                    arguments.output_file, strerror(errno));
+            return 1;
+        }
+    }
+    if (!sri_write(arguments.digest, input_file, output_file)) {
+        fclose(input_file);
         errors_print(stderr);
         return 1;
     }
-    fclose(file);
+    fclose(input_file);
+    if (arguments.output_file != NULL) {
+        fclose(output_file);
+    }
     return 0;
 }
--- c/sri/sri_test.c
+++ c/sri/sri_test.c
@@ -1,28 +1,79 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "c/sri/cmd.h"
 
 struct TestItem {
-    char args[10][BUFSIZ];
+    char** argv;
+    int argc;
+    char* test_string;
+    char* test_string_digest;
     int result;
 };
 
 struct TestItem ARGS[] = {
-    // {.args = {"sdfsdf", "sfsf"}, .result = 1},
+    {
+        .argv = (char*[]){"sri", "sfsf"},
+        .argc = 2,
+        .result = 1,
+    },
+    {
+        .argv = (char*[]){"sri", "--digest", "sha256", "--file",
+                          "test-file.txt", "--output", "test-output.txt"},
+        .argc = 7,
+        .test_string = "Hello World",
+        .test_string_digest =
+            "sha256-pZGm1Av0IEBKARczz7exkNYsZb8LzaMrV7J32a2fFG4=\n",
+        .result = 0,
+    },
+    {
+        .argv =
+            (char*[]){"sri", "--digest", "sha256", "--file", "test-file.txt"},
+        .argc = 5,
+        .test_string = "sdfsdfsfsfsdf",
+        .result = 0,
+    },
 };
 
 int main() {
     int result;
     for (int i = 0; i < (*(&ARGS + 1) - ARGS); i++) {
-        char** args = (char**)(ARGS[i].args);
-        result = cmd_run(1, args);
-        if (result != ARGS[i].result) {
-            fprintf(stderr, "failed test case %d: result %d, should be %d", i,
+        struct TestItem args = ARGS[i];
+        if (args.test_string != NULL) {
+            FILE* test_file = fopen("test-file.txt", "w");
+            if (test_file == NULL) {
+                fprintf(stderr, "could not open file %s: %s\n", "test-file.txt",
+                        strerror(errno));
+                return 1;
+            }
+            fprintf(test_file, "%s", args.test_string);
+            fclose(test_file);
+        }
+        result = cmd_run(args.argc, args.argv);
+        if (result != args.result) {
+            fprintf(stderr, "failed test case %d: result %d, should be %d\n", i,
                     result, ARGS[i].result);
             return 1;
         };
+        if (args.test_string_digest != NULL) {
+            FILE* test_output = fopen("test-output.txt", "r");
+            if (test_output == NULL) {
+                fprintf(stderr, "could not open output file %s: %s\n",
+                        "test-output.txt", strerror(errno));
+                return 1;
+            }
+            char test_output_string[BUFSIZ];
+            fgets(test_output_string, BUFSIZ, test_output);
+            fclose(test_output);
+            if (0 != strcmp(args.test_string_digest, test_output_string)) {
+                fprintf(stderr, "invalid output: should be '%s', got '%s'\n",
+                        args.test_string_digest, test_output_string);
+                return 1;
+            }
+        }
     }
     return 0;
 }
