e4a776e0ebee458826ace272d09e71a535764335
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2025-07-29 20:00:42 +0300

    Add sh leetcode

--- js/leetcode-downloader/leetcode_downloader.js
+++ js/leetcode-downloader/leetcode_downloader.js
@@ -10,86 +10,87 @@
 
 // ==/UserScript==
 
+function saveData(data, fileName) {
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  var blob = new Blob([data], {
+    type: "octet/stream",
+  });
+  var url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function fetchSubmissions(submissions, text, button, offset, limit, lastKey) {
+  button.textContent = `${text}: Fetching ${offset}-${offset + limit}`;
+  GM_xmlhttpRequest({
+    url: `https://leetcode.com/api/submissions/?offset=${offset}&limit=${limit}&lastkey=${lastKey}`,
+    onload: async function (response) {
+      if (response.status !== 200) {
+        button.textContent = `${text}: failed to fetch ${offset}-${offset + limit}: ${response.statusText || response.responseText}`;
+        return;
+      }
+      button.textContent = `${text}: finished ${offset}-${offset + limit}`;
+      const json = JSON.parse(response.responseText);
+      submissions.submissions_dump = submissions.submissions_dump.concat(
+        json.submissions_dump,
+      );
+      if (!json.has_next) {
+        saveSubmissions(submissions, button, text);
+        return;
+      }
+      await sleep(5 * 1000);
+      fetchSubmissions(
+        submissions,
+        text,
+        button,
+        offset + limit,
+        limit,
+        json.lastKey,
+      );
+    },
+  });
+}
+
+function saveSubmissions(submissions, button, text) {
+  saveData(JSON.stringify(submissions), "submissions.json");
+  button.textContent = `${text}: saved submissions`;
+}
+
+function createButton() {
+  const btn = document.createElement("button");
+  const text = "Download submissions";
+  btn.textContent = text;
+  btn.style.position = "fixed";
+  btn.style.bottom = "0";
+  btn.style.right = "0";
+  btn.style.zIndex = "999999";
+  btn.style.padding = "0.5em 1.2em";
+  btn.style.fontSize = "1rem";
+  btn.addEventListener("click", () => {
+    fetchSubmissions(
+      {
+        submissions_dump: [],
+      },
+      text,
+      btn,
+      0,
+      20,
+      "",
+    );
+  });
+  return btn;
+}
+
 (function () {
   "use strict";
 
-  function saveData(data, fileName) {
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    var blob = new Blob([data], {
-      type: "octet/stream",
-    });
-    var url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }
-
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  function fetchSubmissions(submissions, text, button, offset, limit, lastKey) {
-    button.textContent = `${text}: Fetching ${offset}-${offset + limit}`;
-    GM_xmlhttpRequest({
-      url: `https://leetcode.com/api/submissions/?offset=${offset}&limit=${limit}&lastkey=${lastKey}`,
-      onload: async function (response) {
-        if (response.status !== 200) {
-          button.textContent = `${text}: failed to fetch ${offset}-${offset + limit}: ${response.statusText || response.responseText}`;
-          return;
-        }
-        button.textContent = `${text}: finished ${offset}-${offset + limit}`;
-        const json = JSON.parse(response.responseText);
-        submissions.submissions_dump = submissions.submissions_dump.concat(
-          json.submissions_dump,
-        );
-        if (!json.has_next) {
-          saveSubmissions(submissions, button, text);
-          return;
-        }
-        await sleep(5 * 1000);
-        fetchSubmissions(
-          submissions,
-          text,
-          button,
-          offset + limit,
-          limit,
-          json.lastKey,
-        );
-      },
-    });
-  }
-
-  function saveSubmissions(submissions, button, text) {
-    saveData(JSON.stringify(submissions), "submissions.json");
-    button.textContent = `${text}: saved submissions`;
-  }
-
-  function createButton() {
-    const btn = document.createElement("button");
-    const text = "Download submissions";
-    btn.textContent = text;
-    btn.style.position = "fixed";
-    btn.style.bottom = "0";
-    btn.style.right = "0";
-    btn.style.zIndex = "999999";
-    btn.style.padding = "0.5em 1.2em";
-    btn.style.fontSize = "1rem";
-    btn.addEventListener("click", () => {
-      fetchSubmissions(
-        {
-          submissions_dump: [],
-        },
-        text,
-        btn,
-        0,
-        20,
-        "",
-      );
-    });
-    return btn;
-  }
   document.body.appendChild(createButton());
 })();
