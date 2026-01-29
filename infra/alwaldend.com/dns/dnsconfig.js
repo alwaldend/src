873ab660e3dfee0d77593f3ba1a3397583203874
simeonwarren <simeonwarren-git-pzewq@alwaldend.com>
2026-01-29 22:19:45 +0300

    Migrate email to proton.me

--- infra/alwaldend.com/dns/dnsconfig.js
+++ infra/alwaldend.com/dns/dnsconfig.js
@@ -24,19 +24,27 @@ D(
     AAAA("@", "2606:50c0:8001::153"),
     AAAA("@", "2606:50c0:8002::153"),
     AAAA("@", "2606:50c0:8003::153"),
+    CNAME("www", "@", TTL(300)),
 
-    // Protonmail
-    TXT("@", "protonmail-verification=bdcd133d3f472fa17f66328950d02fbeae1bef75", TTL(10800)),
+    // Root txt
+    TXT(
+        "@",
+        [
+            "protonmail-verification=bdcd133d3f472fa17f66328950d02fbeae1bef75",
+            "t-verify=518bd0ae347aa5e3bcca726766106ad3",
+        ],
+        TTL(300)
+    ),
+    TXT("@", "v=spf1 include:_spf.protonmail.ch ~all", TTL(300)),
 
-    // tutanota
-    MX("@", 1, "mail.tutanota.de.", TTL(10800)),
-    TXT("@", "t-verify=518bd0ae347aa5e3bcca726766106ad3", TTL(10800)),
-    TXT("@", "v=spf1 include:spf.tutanota.de -all", TTL(10800)),
-    TXT("_dmarc", "v=DMARC1; p=quarantine; adkim=s", TTL(10800)),
-    CNAME("s1._domainkey", "s1.domainkey.tutanota.de.", TTL(10800)),
-    CNAME("s2._domainkey", "s2.domainkey.tutanota.de.", TTL(10800)),
-    CNAME("_mta-sts", "mta-sts.tutanota.de.", TTL(10800)),
-    CNAME("mta-sts", "mta-sts.tutanota.de.", TTL(10800)),
+
+    // Mail
+    MX("@", 10, "mail.protonmail.ch.", TTL(300)),
+    MX("@", 20, "mailsec.protonmail.ch.", TTL(300)),
+    CNAME("protonmail._domainkey", "protonmail.domainkey.djgwfzcu5fgjtpoijqqomgifmqj6zeiuwdd4mzim4hrxab3zsgwkq.domains.proton.ch.", TTL(300)),
+    CNAME("protonmail2._domainkey", "protonmail2.domainkey.djgwfzcu5fgjtpoijqqomgifmqj6zeiuwdd4mzim4hrxab3zsgwkq.domains.proton.ch.", TTL(300)),
+    CNAME("protonmail3._domainkey", "protonmail3.domainkey.djgwfzcu5fgjtpoijqqomgifmqj6zeiuwdd4mzim4hrxab3zsgwkq.domains.proton.ch.", TTL(300)),
+    TXT("_dmarc", "v=DMARC1; p=quarantine; adkim=s", TTL(300)),
 
     // simplelogin
     MX("simplelogin", 10, "mx1.simplelogin.co.", TTL(10800)),
@@ -73,15 +81,15 @@ D(
         "yandex", 10, "mx.yandex.net.", TTL(21600)
     ),
     TXT(
-        "yandex", "yandex-verification: b83672f59b3dbe16", TTL(10800)
+        "yandex", "yandex-verification: b83672f59b3dbe16", TTL(300)
     ),
     TXT(
-        "yandex", "v=spf1 redirect=_spf.yandex.net", TTL(10800)
+        "yandex", "v=spf1 redirect=_spf.yandex.net", TTL(300)
     ),
     TXT(
         "mail._domainkey.yandex",
         "v=DKIM1; k=rsa; t=s; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCcYzFVgkeDOhaIIkWM8gNQjxVsv0/aXfU+ax5urB5y6hA6lSjRnjRo6tm0bXbkOJf41GmiwMNgdXpwRtzgzAlX1i2aJbtEr4b9jzibEGLQ7Cvqs44bOYES9f/K3ueQpnvdTOJmFqlRReFL7ZrUyDFCoQ7f4+7h4i8s01cCcRrt5wIDAQAB",
-        TTL(10800),
+        TTL(300),
     ),
 
     // dc1
@@ -89,5 +97,5 @@ D(
     // cl1.dc1
     A("pi1.cl1.dc1", "192.168.1.250"),
     AAAA("pi1.cl1.dc1", "fd2e:546d:5738:0:e65d:726e:80d9:fef1"),
-    CNAME("rancher.cl1.dc1", "pi1.cl1.dc1.", TTL(10800)),
+    CNAME("rancher.cl1.dc1", "pi1.cl1.dc1.", TTL(300)),
 );
