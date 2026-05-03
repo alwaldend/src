205407c5098dd281942124311b2e60322d1a37d9
simeonwarren <simeonwarren@alwaldend.com>
2026-05-03 18:23:24 +0000

    Add auth and vault functions to the lua config

--- al.lua
+++ al.lua
@@ -1,19 +1,14 @@
-config({
-    vaults = {
-        {
-            name = "default",
-            config = {
-                address = "https://vault.dc1.alwaldend.com:8200"
-            },
-            tls = {
-                ca_cert = "data/ssl/alwaldend.com_root_ca.crt"
-            }
-        }
+vault({
+    name = "default",
+    config = {
+        address = "https://vault.dc1.alwaldend.com:8200"
     },
-    auth = {
-        {
-            name = "default",
-            token_helper = {}
-        }
+    tls = {
+        ca_cert = "data/ssl/alwaldend.com_root_ca.crt"
     }
+})
+
+auth({
+    name = "default",
+    token_helper = nil
 })
